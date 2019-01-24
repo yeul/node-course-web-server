7f8bbc5eeb9272bc6f2debae5ba3d8c2bec07720
Julia <Julia@Julias-MacBook-Pro.local>
2019-01-24 14:59:24 -0500

    Added Projects page

--- server.js
+++ server.js
@@ -49,6 +49,10 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to fulfill request.'
