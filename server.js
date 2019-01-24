6ecdb53f9ac1a7ef33bc8e39c5224546115436c0
Julia <Julia@Julias-MacBook-Pro.local>
2019-01-24 09:38:42 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}, ${req.path}`
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to fulfill request.'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!')
+});
+
+
